97ecdf5803aa7095b67178f451cbb03e7cf14b1b
Macon Stokes <citostokes@gmail.com>
2018-09-26 08:39:53 -0500

    File Handling and Routing Complete

--- src/app.js
+++ src/app.js
@@ -23,12 +23,45 @@ app.use(express.static(path.join(__dirname, 'public')));
 //  https://expressjs.com/en/api.html#express.static
 // path.join() concatenates the current directory with 'public'
 
-app.get('/', (req, res) => res.render('index', { title: 'Index'}));
+const accountData = fs.readFileSync(
+  path.join(__dirname, 'json', 'accounts.json'), 'utf8'
+);
+// readFileSync returns the contents of the path (first input).
+// this will return a Buffer unless the encoding is specified.
+// in which case it will return a string
+
+const accounts = JSON.parse(accountData);
+// accounts is a javascript object now
+
+const userData = fs.readFileSync(
+  path.join(__dirname, 'json', 'users.json'), 'utf8'
+);
+
+const users = JSON.parse(userData);
+
+
+app.get('/', (req, res) => res.render('index', { title: 'Account Summary', accounts}));
 // routes HTTP GET requests of '/' to the thing after '=>'
 // in this instance the app will respond ('res') with rendering 'index'
 // res.render will render a 'view' (a special name we defined above)
 // res.render is also passing a local variable 'title'
 // the 'view engine' will use that variable, and the app will use ejs
 // to render the view
+
+app.get('/savings', (req, res) => {
+  res.render('account', { account: accounts.savings });
+});
+
+app.get('/checking', (req, res) => {
+  res.render('account', { account: accounts.checking });
+});
+
+app.get('/credit', (req, res) => {
+  res.render('account', { account: accounts.credit });
+});
+
+app.get('/profile', (req, res) => {
+  res.render('profile', { user: users[0] });
+});
 
 app.listen(3000, () => console.log('PS Project running on port 3000!'));
